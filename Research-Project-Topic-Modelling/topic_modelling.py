3b764239ce8ad62b443ac4dac8c4de9503e15e2b
Misbah-Rizaee <misbah.rizaee@gmail.com>
2022-06-24 19:52:39 +0100

    flask added to requirements file

--- Research-Project-Topic-Modelling/topic_modelling.py
+++ Research-Project-Topic-Modelling/topic_modelling.py
@@ -54,11 +54,11 @@ def visualize_the_topics(lda_model, bow_corpus, dictionary):
 
 
 def topic_modelling():
-    # data = pd.read_csv("csv/old_tweetsR.csv")
+    data = pd.read_csv("csv/old_tweets.csv")
 
-    file_names = ['csv/old_tweetsR.csv', 'csv/old_tweetsC.csv']
-    data = pd.concat((pd.read_csv(i, index_col=[0]) for i in file_names)).reset_index(drop=True)
-    data = data.sample(frac=1)  # Shuffle the rows
+    # file_names = ['csv/old_tweetsR.csv', 'csv/old_tweetsC.csv']
+    # data = pd.concat((pd.read_csv(i, index_col=[0]) for i in file_names)).reset_index(drop=True)
+    # data = data.sample(frac=1)  # Shuffle the rows
 
     processed_corpus = preprocess_tweets(data['text'])
 
