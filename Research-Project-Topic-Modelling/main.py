deeae479acd8d1674542170e93fcc17f6d7644c5
Misbah-Rizaee <misbah.rizaee@gmail.com>
2022-06-26 12:43:56 +0100

    word cloud added

--- Research-Project-Topic-Modelling/main.py
+++ Research-Project-Topic-Modelling/main.py
@@ -1,12 +1,14 @@
 from topic_modelling import topic_modelling
 from tweepy_tweets import collect_tweets
 from snscrape_tweets import collect_old_tweets
+from word_cloud_class import word_cloud
 
 
 def main():
     # collect_tweets()
     # collect_old_tweets()
-    topic_modelling()
+    # topic_modelling()
+    word_cloud()
 
 
 if __name__ == '__main__':
---
+++ Research-Project-Topic-Modelling/word_cloud_class.py
@@ -0,0 +1,35 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+
+def preprocess_tweets(corpus):
+    # Removing stopwords from the tweets
+    stop_words = stopwords.words('english')
+    stop_words.extend(['amp', 'even', 'maybe', 'another', 'getting', 'yet', 'lot', 'n\'t', 'seem', 'thing', 'anything'])
+    processed_corpus = ""
+    lemmatizer = WordNetLemmatizer()
+
+    for document in corpus:
+        word_list = [w for w in word_tokenize(document) if (w not in stop_words)]  # Remove stop words
+        word_list = [lemmatizer.lemmatize(w) for w in word_list if len(w) > 2]  # lemmatized the words
+        processed_corpus += " ".join(word_list) + " "
+
+    return processed_corpus
+
+
+def word_cloud():
+    data = pd.read_csv("csv/old_tweets.csv")
+    processed_corpus = preprocess_tweets(data['text'])
+
+    # Creating the word cloud object
+    wordcloud = WordCloud(width=1000, height=1000, margin=0, background_color='white',
+                          colormap='tab10').generate(processed_corpus)
+
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.margins(x=0, y=0)
+    plt.show()
